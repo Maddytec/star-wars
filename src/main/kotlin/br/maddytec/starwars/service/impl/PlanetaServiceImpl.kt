ee38816f676de5a67987746a65756857ab8e7927
Madson Araujo da Silva <madson.silva@HOTBHZA001317.local>
2021-09-25 10:39:45 -0300

    Adicionado cache nas consultas de planetas

--- src/main/kotlin/br/maddytec/starwars/service/impl/PlanetaServiceImpl.kt
+++ src/main/kotlin/br/maddytec/starwars/service/impl/PlanetaServiceImpl.kt
@@ -3,6 +3,8 @@ package br.maddytec.starwars.service.impl
 import br.maddytec.starwars.model.Planeta
 import br.maddytec.starwars.repository.PlanetaRepository
 import br.maddytec.starwars.service.PlanetaService
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
@@ -11,16 +13,20 @@ import java.util.*
 @Service
 class PlanetaServiceImpl(val planetaRepository: PlanetaRepository): PlanetaService {
 
+    @CacheEvict("planetas", allEntries = true)
     override fun save(planeta: Planeta) = planetaRepository.save(planeta)
 
+    @CacheEvict("planetas", allEntries = true)
     override fun update(id: Long, planeta: Planeta) = planetaRepository.save(planeta.copy(id = id))
 
+    @Cacheable("planetas")
     override fun findAll(): List<Planeta> = planetaRepository.findAll()
 
     override fun findById(id: Long): Optional<Planeta> = planetaRepository.findById(id)
 
+    @Cacheable("planetas")
     override fun getPlanetaFiltro(nome: String, clima: String, terreno: String): List<Planeta>? {
-        return planetaRepository.findAll()
+        return this.findAll()
             .filter {
                 it.nome.contains(nome, true)
                 && it.clima.contains(clima, true)
@@ -28,6 +34,7 @@ class PlanetaServiceImpl(val planetaRepository: PlanetaRepository): PlanetaServi
             }
     }
 
+    @CacheEvict("planetas", allEntries = true)
     override fun delete(id: Long) = planetaRepository.deleteById(id)
 
 
